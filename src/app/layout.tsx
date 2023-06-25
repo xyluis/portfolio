e9a80fab12bed88b605a95049a4bd4df8d10d85b
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2023-06-25 13:51:50 -0400

    feat: improve metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,28 @@
 import { ReactNode } from 'react'
+import { Metadata } from 'next'
 
 import './globals.css'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
 import { TailwindIndicator } from '@/components/TailwindIndicator'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Luís',
   description: "Luís's personal website built with Next.js and Tailwind CSS.",
+  openGraph: {
+    type: 'website',
+    title: 'Luís',
+    images: {
+      url: 'https://i.imgur.com/JrAR7s2.png',
+    },
+  },
+  twitter: {
+    title: 'Luís',
+    description: "Luís's personal website built with Next.js and Tailwind CSS.",
+    images: {
+      url: 'https://i.imgur.com/JrAR7s2.png',
+    },
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
