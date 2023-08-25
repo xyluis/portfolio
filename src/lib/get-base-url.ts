61112e34f4b9a29fbb10f0fc77d522e007d76251
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2023-08-25 01:03:43 -0400

    fix: update base url

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -2,6 +2,6 @@ import { cache } from 'react'
 
 export const getBaseUrl = cache(() => {
   return process.env.VERCEL_URL
-    ? `https://www.xyluis.tech`
+    ? `https://xyluis.tech`
     : `http://localhost:${process.env.PORT ?? 3000}`
 })
