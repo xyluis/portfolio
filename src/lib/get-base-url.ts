4f352728ee1516fddfd309154b61f0e73c82f039
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2023-08-25 01:10:46 -0400

    porra

--- src/lib/get-base-url.ts
+++ src/lib/get-base-url.ts
@@ -2,6 +2,6 @@ import { cache } from 'react'
 
 export const getBaseUrl = cache(() => {
   return process.env.VERCEL_URL
-    ? `https://xyluis.tech`
+    ? `https://www.xyluis.tech`
     : `http://localhost:${process.env.PORT ?? 3000}`
 })
